b4a5f531b32c9290e9ca56370c57fb44ad695d70
JRausell <jrausell@gmail.com>
2023-11-03 19:34:53 +0100

    x

--- app/actions/signUpPrisma.ts
+++ app/actions/signUpPrisma.ts
@@ -9,17 +9,28 @@ export async function signUpPrisma(email: string) {
       message: "Please enter a valid email",
     };
 
-  return {
-    error: true,
-    message: "Please enter a valid email",
-  };
+  try {
+    // insert email in Subscription table
+    const subscription = await prisma.subscription.create({
+      data: {
+        email,
+      },
+    });
 
-  // insert email in Subscription table
-  const subscription = await prisma.subscription.create({
-    data: {
-      email,
-    },
-  });
+    if ("error" in subscription) {
+      return {
+        success: false,
+        message: "Error create",
+        errm: JSON.stringify(subscription?.error ?? subscription),
+      };
+    }
 
-  return subscription;
+    return subscription;
+  } catch (e) {
+    return {
+      success: false,
+      message: "Error signing up, possible duplicate email",
+      errm: JSON.stringify(e),
+    };
+  }
 }
