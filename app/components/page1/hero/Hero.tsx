772e4f586558d462bcd62166f26e2dee642b90e2
JRausell <jrausell@gmail.com>
2023-11-09 15:53:37 +0100

    mobile

--- app/components/page1/hero/Hero.tsx
+++ app/components/page1/hero/Hero.tsx
@@ -8,10 +8,10 @@ const Hero = () => {
   return (
     <>
       <div className="relative flex flex-row justify-center items-center ">
-        <div className="relative max-w-4xl p-8 xl:p-0 2xl:mb-8 flex flex-row">
+        <div className="relative w-full lg:max-w-4xl p-8 xl:p-0 2xl:mb-8 flex flex-col lg:flex-row">
           {/* text */}
-          <div className="w-3/5">
-            <div className="grow-0 shrink h-auto flex flex-row justify-start items-center">
+          <div className="w-full lg:w-3/5">
+            <div className="grow-0 shrink h-auto flex flex-col lg:flex-row justify-start items-center">
               <h1 className="relative text-6xl font-black bg-gradient-to-br from-cyan-400 via-purple-300 to-fuchsia-700 text-transparent bg-clip-text">
                 <span className="text-blue-300">$</span>
                 <span className="text-pink-300">0</span>MRR
@@ -21,7 +21,7 @@ const Hero = () => {
               </span>
             </div>
             {/* */}
-            <div className="mt-4 font-extralight text-2xl flex flex-col justify-between max-w-[780px]">
+            <div className="mt-4 font-extralight text-xl lg:text-2xl flex flex-col justify-between w-full lg:max-w-[780px]">
               <div>Tired of everything being messy?</div>
               <div>
                 Keep track of the things that{" "}
@@ -44,15 +44,25 @@ const Hero = () => {
                 </span>
                 , stay on track and finish it.
               </div>
+              {/* image mobile */}
+              <div
+                className="relative bottom-0 right-0 w-full h-60 flex flex-col justify-end"
+                style={{
+                  backgroundImage: "url(/images/search.png)",
+                  backgroundPosition: "top",
+                  backgroundRepeat: "no-repeat",
+                  backgroundSize: "contain",
+                }}
+              ></div>
               {/* form */}
-              <div className=" mt-20 flex flex-col justify-start w-4/5">
+              <div className=" mt-20 flex flex-col justify-start w-full lg:w-4/5">
                 <SignUp />
               </div>
             </div>
           </div>
           {/* image */}
           <div
-            className="absolute bottom-0 right-0 w-1/2 h-full  flex flex-row justify-end"
+            className="relative lg:absolute bottom-0 right-0 w-full lg:w-1/2 h-full hidden lg:flex flex-row justify-end"
             style={{
               backgroundImage: "url(/images/search.png)",
               backgroundPosition: "top",
